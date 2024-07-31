1930b9be4c5ee2be753c8a6383b02bdeb9a032a9
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-31 12:10:07 +0300

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 """Filter Logger"""
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
-    """Mask value of fileds with xxxx"""
+    """Mask value of fields with xxxx"""
     pattern = f"({'|'.join(map(re.escape, fields))})=.+?({separator}|$)"
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{m.group(2)}", message)
+    return re.sub(pattern,
+                  lambda m: f"{m.group(1)}={redaction}{m.group(2)}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        filter_datum(self.fields, self.REDACTION,
+                     super().format(record), self.SEPARATOR)
