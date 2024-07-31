05c0135d22572b2475d1a99ec4308a66ac1e3654
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-31 12:25:42 +0300

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,27 +2,46 @@
 """Filter Logger"""
 import re
 import logging
+from typing import List, Tuple
+
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields, redaction, message, separator):
-    """Mask value of fields with xxxx"""
+def get_logger() -> logging.Logger:
+    """Creates and returns a logger named 'user_data'"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Mask value of fields with redaction"""
     pattern = f"({'|'.join(map(re.escape, fields))})=.+?({separator}|$)"
     return re.sub(pattern,
                   lambda m: f"{m.group(1)}={redaction}{m.group(2)}", message)
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        original_message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
-                     super().format(record), self.SEPARATOR)
+                            original_message, self.SEPARATOR)
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -6,17 +6,24 @@ import logging
 
 filter_datum = __import__('filtered_logger').filter_datum
 RedactingFormatter = __import__('filtered_logger').RedactingFormatter
+get_logger = __import__('filtered_logger').get_logger
+PII_FIELDS = __import__('filtered_logger').PII_FIELDS
 
+
+##TASK 0 TESTING
 fields = ["password", "date_of_birth"]
 messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
 
 for message in messages:
     print(filter_datum(fields, 'xxx', message, ';'))
 
-
-
-
+##TASK 1 TESTING
 message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
 log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
 formatter = RedactingFormatter(fields=("email", "ssn", "password"))
 print(formatter.format(log_record))
+
+
+## TASK 2 TESTING
+print(get_logger.__annotations__.get('return'))
+print("PII_FIELDS: {}".format(len(PII_FIELDS)))
