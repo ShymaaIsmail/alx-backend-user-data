fc0736d130a98ba93089c611b0149a140b5efc2d
shymaaismail <shymaa.m.ismail@gmail.com>
2024-08-05 11:35:06 +0300

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> str:
     """unauthorized route"""
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """forbidden route"""
+    abort(403)
