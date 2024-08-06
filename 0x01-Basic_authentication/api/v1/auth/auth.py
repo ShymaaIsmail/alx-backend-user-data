4e3d3e47610a950566d050169cc21859d92fbf97
shymaaismail <shymaa.m.ismail@gmail.com>
2024-08-06 13:09:32 +0300

    "Added import of re module and modified excluded_paths check in Auth class"

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Auth Module for the API"""
 
 
+import re
 from typing import List, TypeVar
 from flask import request, jsonify
 
@@ -16,8 +17,9 @@ class Auth():
             return True
         if path[-1] != '/':
             path += '/'
-        if path in excluded_paths:
-            return False
+        for excluded_path in excluded_paths:
+            if(re.match(excluded_path, path)):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
