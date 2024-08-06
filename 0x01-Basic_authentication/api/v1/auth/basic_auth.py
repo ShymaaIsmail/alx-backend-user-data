0a9f7b4ef11a54946015971a30bce34d4a0b4b23
shymaaismail <shymaa.m.ismail@gmail.com>
2024-08-06 12:24:02 +0300

    "Refactored BasicAuth class and updated imports in views/__init__.py"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,7 +9,7 @@ class BasicAuth(Auth):
     """Basic Auth Class"""
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """Extract the Base64 part of the Authorization header for Basic Authentication."""
+        """Extract the Base64 part of the Auth header for Basic Auth."""
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
@@ -22,13 +22,11 @@ class BasicAuth(Auth):
         """Decode a Base64 string and return the UTF-8 decoded value."""
         if base64_authorization_header is None:
             return None
-
         if not isinstance(base64_authorization_header, str):
             return None
-
         try:
             # Decode the Base64 string
             decoded_bytes = base64.b64decode(base64_authorization_header)
-            return decoded_bytes.decode('utf-8')  # Decode bytes to UTF-8 string
+            return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
