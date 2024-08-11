7343056bf2bcdff696aeb93940f4e499f5e65863
shymaaismail <shymaa.m.ismail@gmail.com>
2024-08-11 11:25:48 +0300

    "Added current_user method to SessionAuth class and imported necessary modules"

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """SessionAuth"""
 
+from typing import TypeVar
 import uuid
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -30,3 +32,15 @@ class SessionAuth(Auth):
 
         # Retrieve the user_id associated with the session_id
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns a User instance based on a cookie value"""
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
