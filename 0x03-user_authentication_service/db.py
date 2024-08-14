44a687006274d54c4553be636b30bd2073e73157
shymaaismail <shymaa.m.ismail@gmail.com>
2024-08-14 12:27:06 +0300

    fix importing InvalidRequestError

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,9 +2,10 @@
 """DB module"""
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, InvalidRequestError
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import User, Base
 
